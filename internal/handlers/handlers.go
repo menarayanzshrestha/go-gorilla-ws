b067d2059bcb481987d9c6d612be9d0a9701925f
Narayan-Stha <benarayanz@gmail.com>
2022-02-05 18:46:19 +0545

    feat: added users connected list in FE

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sort"
 
 	"github.com/CloudyKit/jet/v6"
 	"github.com/gorilla/websocket"
@@ -36,11 +37,12 @@ type WebSocketConnection struct {
 	*websocket.Conn
 }
 
-//response fback from ws
+//response back from ws
 type WsJsonResponse struct {
-	Action      string `json:"action"`
-	Message     string `json:"message"`
-	MessageType string `json:"message_type"`
+	Action         string   `json:"action"`
+	Message        string   `json:"message"`
+	MessageType    string   `json:"message_type"`
+	ConnectedUsers []string `json:"connected_users"`
 }
 
 type WsPayload struct {
@@ -99,10 +101,32 @@ func ListenToWsChannel() {
 
 	for {
 		e := <-wsChan
+
+		switch e.Action {
+		case "username":
+			//get all users and broadcast
+			clients[e.Conn] = e.Username
+			users := getUserList()
+
+			response.Action = "list_users"
+			response.ConnectedUsers = users
+			broadcastToAll(response)
+
+		}
+
 		response.Action = "Got here"
 		response.Message = fmt.Sprintf("Some message, and action was %s", e.Action)
 		broadcastToAll(response)
 	}
+}
+
+func getUserList() []string {
+	var userList []string
+	for _, x := range clients {
+		userList = append(userList, x)
+	}
+	sort.Strings(userList)
+	return userList
 }
 
 func broadcastToAll(response WsJsonResponse) {
