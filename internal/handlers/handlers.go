ed324d105cb158e41b6144f4f2af759f90c6fa96
Narayan-Stha <benarayanz@gmail.com>
2022-02-07 22:36:51 +0545

    fix: optimixation and notifier

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -35,6 +35,8 @@ func Home(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// WebSocketConnection is a wrapper for our websocket connection, in case
+// we ever need to put more data into the struct
 type WebSocketConnection struct {
 	*websocket.Conn
 }
@@ -78,6 +80,8 @@ func WsEndpoint(w http.ResponseWriter, r *http.Request) {
 	go ListenForWs(&conn)
 }
 
+// ListenForWs is a goroutine that handles communication between server and client, and
+// feeds data into the wsChan
 func ListenForWs(conn *WebSocketConnection) {
 	defer func() {
 		if r := recover(); r != nil {
@@ -98,6 +102,8 @@ func ListenForWs(conn *WebSocketConnection) {
 	}
 }
 
+// ListenToWsChannel is a goroutine that waits for an entry on the wsChan, and handles it according to the
+// specified action
 func ListenToWsChannel() {
 	var response WsJsonResponse
 
@@ -129,6 +135,7 @@ func ListenToWsChannel() {
 	}
 }
 
+// getUserList returns a slice of strings containing all usernames who are currently online
 func getUserList() []string {
 	var userList []string
 	for _, x := range clients {
@@ -140,10 +147,12 @@ func getUserList() []string {
 	return userList
 }
 
+// broadcastToAll sends ws response to all connected clients
 func broadcastToAll(response WsJsonResponse) {
 	for client := range clients {
 		err := client.WriteJSON(response)
 		if err != nil {
+			// the user probably left the page, or their connection dropped
 			log.Println("websocket err")
 			_ = client.Close()
 			delete(clients, client)
